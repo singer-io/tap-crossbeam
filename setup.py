707edb982b83b6dc696b6ad5ee91a05bd4d7c121
Andrew Madonna <andrew.w.madonna@gmail.com>
2021-03-01 16:39:11 -0500

    Add install_requires packages

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(name='tap-crossbeam',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_crossbeam'],
       install_requires=[
+          'backoff==1.8.0',
+          'ratelimit==2.2.1',
+          'requests==2.25.1',
+          'singer-python==5.10.0'
       ],
       entry_points='''
           [console_scripts]
