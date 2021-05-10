dc17b1efcb49b512469712940852db709576dbfb
Buck Ryan <buck@buckryan.com>
2021-05-10 12:12:48 -0400

    Refactor discover code

--- tap_crossbeam/discover.py
+++ tap_crossbeam/discover.py
@@ -8,21 +8,23 @@ from tap_crossbeam.endpoints import ENDPOINTS_CONFIG
 SCHEMAS = {}
 FIELD_METADATA = {}
 
+
 def get_pk(stream_name, endpoints=None):
     if not endpoints:
         endpoints = ENDPOINTS_CONFIG
-
     for endpoint_stream_name, endpoint in endpoints.items():
         if stream_name == endpoint_stream_name:
             return endpoint['pk']
-
         if 'children' in endpoint:
             pk = get_pk(stream_name, endpoints=endpoint['children'])
             if pk:
                 return pk
+    return None
+
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
 
 def get_schemas():
     global SCHEMAS, FIELD_METADATA
@@ -39,13 +41,13 @@ def get_schemas():
         stream_name = file_name[:-5]
         with open(os.path.join(schemas_path, file_name)) as data_file:
             schema = json.load(data_file)
-            
+
         SCHEMAS[stream_name] = schema
 
         pk = get_pk(stream_name)
 
         metadata = []
-        for prop, json_schema in schema['properties'].items():
+        for prop, _ in schema['properties'].items():
             if prop in pk:
                 inclusion = 'automatic'
             else:
@@ -60,104 +62,73 @@ def get_schemas():
 
     return SCHEMAS, FIELD_METADATA
 
+
+def _field_jschema_type(field):
+    cb_type = field['data_type']
+    cb_pg_type = field['pg_data_type']
+    if cb_type == 'datetime':
+        return ('string', 'date-time')
+    if cb_type == 'number':
+        if cb_pg_type == 'bigint':
+            return ('integer', None)
+        return ('number', None)
+    if cb_type == 'boolean':
+        return ('boolean', None)
+    return ('string', None)
+
+
+def _add_fields_to_properties(properties, source):
+    for field in source['fields']:
+        field_name = field['column']  # FIXME this should just use display name
+        json_type, json_format = _field_jschema_type(field)
+        if field_name in properties:
+            if json_type not in properties[field_name]['type']:
+                properties[field_name]['type'].append(json_type)
+        else:
+            json_schema = {'type': ['null', json_type]}
+            if json_format:
+                json_schema['format'] = json_format
+            properties[field_name] = json_schema
+
+
+def _add_fields_to_metadata(metadata, source):
+    for field in source['fields']:
+        field_name = field['column']  # FIXME this should just use display name
+        if field_name in metadata:
+            if field['display_name'] not in metadata[field_name]['tap-crossbeam.display_names']:
+                metadata[field_name]['tap-crossbeam.display_names'].append(field['display_name'])
+            # if field['nickname'] not in metadata[field_name]['tap-crossbeam.nicknames']:
+            #     metadata[field_name]['tap-crossbeam.nicknames'].append(field['nickname'])
+        else:
+            metadata[field_name] = {
+                'inclusion': 'available',
+                'tap-crossbeam.display_names': [field['display_name']],
+                # 'tap-crossbeam.nicknames': [field['nickname']]
+            }
+
+
 def get_schema_from_source(streams, source):
     stream_name = source['mdm_type']
-    existing_schema = streams.get(stream_name)
-
-    if existing_schema:
-        properties = existing_schema['properties']
-        metadata = existing_schema['metadata']
-    else:
-        properties = {
-            '__xb_crossbeam_id': {
-                'type': [
-                    'string'
-                ]
-            },
-            '__xb_record_id': {
-                'type': [
-                    'string'
-                ]
-            },
-            '__xb_record_type': {
-                'type': [
-                    'string'
-                ]
-            },
-            '__xb_record_name': {
-                'type': [
-                    'null',
-                    'string'
-                ]
-            },
-            '__xb_updated_at': {
-                'type': [
-                    'null',
-                    'string'
-                ],
-                'format': 'date-time'
-            }
-        }
-        metadata = {
+    streams[stream_name] = streams.get(stream_name) or {
+        'properties': {
+            '__xb_crossbeam_id': {'type': ['string']},
+            '__xb_record_id': {'type': ['string']},
+            '__xb_record_type': {'type': ['string']},
+            '__xb_record_name': {'type': ['null', 'string']},
+            '__xb_updated_at': {'type': ['null', 'string'], 'format': 'date-time'},
+        },
+        'metadata': {
             '__table__': {
                 'tap-crossbeam.is_partner_data': True,
                 'tap-crossbeam.schema': source['schema'],
                 'tap-crossbeam.table': source['table'],
-                'tap-crossbeam.mdm_type': source['mdm_type']
+                'tap-crossbeam.mdm_type': source['mdm_type'],
             }
-        }
+        },
+    }
+    _add_fields_to_properties(streams[stream_name]['properties'], source)
+    _add_fields_to_metadata(streams[stream_name]['metadata'], source)
 
-    for field in source['fields']:
-        field_name = field['column']
-
-        cb_type = field['data_type']
-        cb_pg_type = field['pg_data_type']
-
-        json_type = 'string'
-        json_format = None
-
-        if cb_type == 'datetime':
-            json_format = 'date-time'
-        elif cb_type == 'number':
-            if cb_pg_type == 'bigint':
-                json_type = 'integer'
-            else:
-                json_type = 'number'
-        elif cb_type == 'boolean':
-            json_type = 'boolean'
-
-        if field_name in properties:
-            if json_type not in properties[field_name]['type']:
-                properties[field_name]['type'].append(json_type)
-
-            if field['display_name'] not in metadata[field_name]['tap-crossbeam.display_names']:
-                metadata[field_name]['tap-crossbeam.display_names'].append(field['display_name'])
-
-            if field['nickname'] not in metadata[field_name]['tap-crossbeam.nicknames']:
-                metadata[field_name]['tap-crossbeam.nicknames'].append(field['nickname'])
-        else:
-            json_schema = {
-                'type': [
-                    'null',
-                    json_type
-                ]
-            }
-
-            if json_format:
-                json_schema['format'] = json_format
-
-            properties[field_name] = json_schema
-            metadata[field_name] = {
-                'inclusion': 'available',
-                'tap-crossbeam.display_names': [field['display_name']],
-                'tap-crossbeam.nicknames': [field['nickname']]
-            }
-    
-    if not existing_schema:
-        streams[stream_name] = {
-            'properties': properties,
-            'metadata': metadata
-        }
 
 # this function iterates over all sources and creates a table per mdm_type
 # an mdm_type may come from multiple sources
@@ -199,6 +170,7 @@ def get_partner_data_schemas(client):
             'metadata': metadata
         }
     return singer_streams
+
 
 def discover(client):
     schemas, field_metadata = get_schemas()
